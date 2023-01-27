c54a9b41cbd782b813fb8ca8a46163ec4af12a87
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-01-28 01:26:16 +0530

    Update list-tags.ps1

--- list-tags.ps1
+++ list-tags.ps1
@@ -1,6 +1,9 @@
 $mg = Get-AzManagementGroup -GroupName '296a2a1e-c1ca-475f-aaea-3d1bcf9eab8d'
 $subscriptions = Get-AzSubscription | Where-Object { $_.TenantId -eq $mg.TenantId }
 
+# Connect to Azure using an interactive login
+#Connect-AzAccount
+
 # Get all subscriptions
 $subscriptions = Get-AzSubscription
 
@@ -10,10 +13,11 @@ $subscriptionTags = @()
 foreach ($subscription in $subscriptions) {
     # Check if the subscription has tags
     if($subscription.Tags -ne $null) {
-        # Iterate through each tag and add the subscription ID, tag name, and tag value to the array
+        # Iterate through each tag and add the subscription ID, tag name, subscription name and tag value to the array
         foreach ($tag in $subscription.Tags.GetEnumerator()) {
             $subscriptionTag = [PSCustomObject]@{
                 SubscriptionId = $subscription.Id
+                SubscriptionName = $subscription.Name
                 TagName = $tag.Key
                 TagValue = $tag.Value
             }
@@ -22,6 +26,7 @@ foreach ($subscription in $subscriptions) {
     }else {
         $subscriptionTag = [PSCustomObject]@{
                 SubscriptionId = $subscription.Id
+                SubscriptionName = $subscription.Name
                 TagName = "null"
                 TagValue = "null"
             }
