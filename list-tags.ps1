6207728fb94f1b26d794efe2bd748330e1a9c121
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-01-27 17:14:39 +0530

    Create list-tags.ps1

---
+++ list-tags.ps1
@@ -0,0 +1,21 @@
+$mg = Get-AzManagementGroup -GroupName '296a2a1e-c1ca-475f-aaea-aaaaaaaaa'
+$subscriptions = Get-AzSubscription | Where-Object { $_.TenantId -eq $mg.TenantId }
+
+# Create an empty array to store the output data
+$output = @()
+
+# Iterate through the subscriptions, retrieve their tags, and add them to the output array
+foreach ($subscription in $subscriptions) {
+    $subsTags = (Get-AzSubscription -SubscriptionId $subscription.Id).Tags
+    foreach ($tag in $subsTags.GetEnumerator()) {
+        $subscriptionTag = [PSCustomObject]@{
+            SubscriptionID = $subscription.Id
+            TagKey         = $tag.Key
+            TagValue       = $tag.Value
+        }
+        $output += $subscriptionTag
+    }
+}
+
+# Format the output as a table
+$output | Format-Table -AutoSize
